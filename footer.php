e7c5bf14e1c50e6d8cd03f13814f993ad97f5f9e
Brice Tchamou <michaeltchamou393@gmail.com>
2024-05-29 15:46:22 +0100

    footer page

---
+++ footer.php
@@ -0,0 +1,67 @@
+<div class="row mt-4 mb-2 mx-1 bg-dark" style=" border-radius: 15px 15px 0 0;  " >
+            <div class="col-12 col-sm-12 ">
+                <br>
+
+            <div class="row mt-3">
+                <div class="col-sm-4 text-white">
+                    <h4 class="text-center">Contactez-Nous</h4>
+                    <p> <a href="" class="text-white page-link text-center" >Email : WellFood@gmail.com</a></p>
+                    <p> <a href="" class="text-white page-link text-center">Telephone : 690384751</a></p>
+                </div>
+                <div class="col-sm-4 text-white text-center text-decoration-none">
+                    <h4 class="text-center">A propos de WellFood</h4>
+                    <p> <a href="" class="text-white page-link">Condition D'utilisation</a></p>
+                    <p> <a href="" class="text-white page-link">Politique de confidentialité</a></p>
+                    <p> <a href="" class="text-white page-link">Accéssibilité</a></p>
+
+
+                </div>
+                <div class="col-sm-4 text-white">
+                    <h4 class="text-center"> Nos services</h4>
+                    <p> <a href="" class="text-white page-link text-center">Restauration</a></p>
+                    <p> <a href="" class="text-white page-link text-center">Formation</a></p>
+
+
+
+                </div>
+
+        
+            </div>
+
+            <div class="row mt-3"> 
+                <div class="col-sm-12 col-12 text-white">
+
+                    <H3 class="text-center">Suivez-Nous</H3>
+                    <div class="row">
+                        <div class="col-sm-4"></div>
+
+                        <div class="col-sm-4">
+                            <div class=" d-flex text-center mx-4 my-3">
+
+                            <a href="" class="page-link "><i class="fa-brands fa-whatsapp fa-3x"></i></a>
+
+                            <a href="" class="page-link mx-3 "><i class="fa-brands fa-facebook fa-3x"></i></a>
+                            <a href="" class="page-link mx-3"><i class="fa-brands fa-linkedin fa-3x"></i></a>
+                            <a href="" class="page-link mx-3"><i class="fa-brands fa-instagram fa-3x"></i></a>
+                            <a href="" class="page-link mx-3"><i class="fa-brands fa-twitter fa-3x"></i></a>
+
+                            </div>
+
+
+
+                            
+                        </div>
+
+                        <div> </div>
+                    </div>
+
+                </div>
+            </div>
+
+            </div>
+          
+
+
+
+
+        </div>
